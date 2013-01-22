6043549d2f944a03ea143e1838ae3b5174f91afa
MITSUNARI Shigeo <herumi@nifty.com>
2013-01-22 11:46:45 +0900

    separate xorshift

--- include/cybozu/nlp/random.hpp
+++ include/cybozu/nlp/random.hpp
@@ -6,56 +6,43 @@
 	Copyright (C) 2007 Cybozu Labs, Inc., all rights reserved.
 	@author MITSUNARI Shigeo
 */
-#include <cybozu/inttype.hpp>
+#include <cybozu/xorshift.hpp>
 
 namespace cybozu { namespace nlp {
 
 /*
-	xor shift
+	use xor shift
 */
 class UniformRandomGenerator {
 	double a_;
 	double b_;
-	uint32_t x_, y_, z_, w_;
+	cybozu::XorShift rg;
 public:
 	/* generate uniform random value in [a, b) */
 	explicit UniformRandomGenerator(double a = 0, double b = 1, int seed = 0)
 		: a_(a)
 		, b_(b)
+		, rg(seed)
 	{
-		init(seed);
 	}
 	void init(int seed = 0)
 	{
-		x_ = 123456789 + seed;
-		y_ = 362436069;
-		z_ = 521288629;
-		w_ = 88675123;
+		rg.init(seed);
 	}
 	/* [0, 2^32) random number */
-	uint32_t get()
-	{
-		unsigned int t = x_ ^ (x_ << 11);
-		x_ = y_; y_ = z_; z_ = w_;
-		return w_ = (w_ ^ (w_ >> 19)) ^ (t ^ (t >> 8));
-	}
-	uint32_t operator()() { return get(); }
-	uint32_t get32() { return get(); }
-	uint64_t get64()
-	{
-		uint32_t a = get();
-		uint32_t b = get();
-		return (uint64_t(a) << 32) | b;
-	}
+	uint32_t operator()() { return rg.get32(); }
+	uint32_t get32() { return rg.get32(); }
+	uint64_t get64() { return rg.get64(); }
 	/* [a, b) random number */
 	double getDouble()
 	{
-		unsigned int x = get() >> 5;
-		unsigned int y = get() >> 6;
+		uint32_t x = get32() >> 5;
+		uint32_t y = get32() >> 6;
 		double z = (x * double(1U << 26) + y) * (1.0 / double(1LL << 53));
 		return (b_ - a_) * z + a_;
 	}
 };
+
 /*
 	normal random generator
 */
