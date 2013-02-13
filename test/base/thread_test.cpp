400affaf79248fd0d187c5e1671674d851236139
MITSUNARI Shigeo <herumi@nifty.com>
2013-02-13 15:44:24 +0900

    fix return code for begin/joint

--- test/base/thread_test.cpp
+++ test/base/thread_test.cpp
@@ -9,18 +9,23 @@ int s_count = 0;
 
 class ThreadTest : public cybozu::ThreadBase {
 	cybozu::Mutex& mutex_;
+	int num_;
 public:
-	ThreadTest(cybozu::Mutex& mutex)
+	ThreadTest(cybozu::Mutex& mutex, int num)
 		: mutex_(mutex)
+		, num_(num)
 	{
+		printf("make %d\n", num_);
 	}
 
 	void threadEntry()
 	{
+		printf("start %d\n", num_);
 		for (int i = 0; i < N; i++) {
 			cybozu::AutoLock al(mutex_);
 			s_count++;
 		}
+		printf("end %d\n", num_);
 	}
 };
 
@@ -28,14 +33,14 @@ public:
 CYBOZU_TEST_AUTO(autoLock)
 {
 	cybozu::Mutex mutex_;
-	ThreadTest t1(mutex_);
-	ThreadTest t2(mutex_);
-	t1.beginThread();
-	t2.beginThread();
+	ThreadTest t1(mutex_, 1);
+	ThreadTest t2(mutex_, 2);
+	if (!t1.beginThread()) puts("err thread 1");
+	if (!t2.beginThread()) puts("err thread 2");
+	cybozu::Sleep(1000);
 	/*
 		verity number of increment
 	*/
-	puts("check mutex");
 	puts("start main");
 	for (int i = 0; i < N; i++) {
 		cybozu::AutoLock al(mutex_);
@@ -43,8 +48,8 @@ CYBOZU_TEST_AUTO(autoLock)
 	}
 	puts("end main");
 
-	t1.joinThread();
-	t2.joinThread();
+	if (!t1.joinThread()) puts("err join 1");
+	if (!t2.joinThread()) puts("err join 2");
 	cybozu::mfence();
 	CYBOZU_TEST_EQUAL(s_count, N * 3);
 }
