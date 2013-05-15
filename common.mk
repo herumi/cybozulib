14c181bc2e7b12229eedcd343fa22ff6e2e22684
MITSUNARI Shigeo <herumi@nifty.com>
2013-05-15 21:46:56 +0900

    remove comments; add -lcrypto for openssl 1.0

--- common.mk
+++ common.mk
@@ -10,11 +10,10 @@ AR = ar r
 MKDIR=mkdir -p
 RM=rm -fr
 CFLAGS_OPT += -O3 -fomit-frame-pointer -DNDEBUG
-#CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wconversion -Wfloat-equal -Wpointer-arith #-Wswitch-enum -Wstrict-aliasing=2
 CFLAGS_WARN=-Wall -Wextra -Wformat=2 -Wcast-qual -Wcast-align -Wwrite-strings -Wfloat-equal -Wpointer-arith #-Wswitch-enum -Wstrict-aliasing=2
 CFLAGS = -g -D_FILE_OFFSET_BITS=64 -msse4.2
 CFLAGS+=$(CFLAGS_WARN)
-LDFLAGS += -lz -lpthread -lssl -lcrypto -lmecab -lboost_regex
+LDFLAGS += -lz -lpthread -lssl  -lmecab -lboost_regex
 BIT=64
 
 DEBUG=1
@@ -34,8 +33,6 @@ endif
 
 TOPDIR=$(shell 'pwd' | sed "s@cybozulib/.*@cybozulib/@")
 EXTDIR=$(TOPDIR)../cybozulib_ext/
-#CFLAGS+= -I$(TOPDIR)include -I$(EXTDIR)icu4c/icu/include -I$(EXTDIR)openssl/openssl/include
-#LDFLAGS+= -L$(TOPDIR)lib -lssl -lcrypto -ldl -licuuc -licudata -licui18n -lz -Wl,-rpath,'$$ORIGIN/../lib'
 CFLAGS+= -I$(TOPDIR)include -m$(BIT)
 LDFLAGS+= -L$(TOPDIR)lib -m$(BIT) -Wl,-rpath,'$$ORIGIN/../lib'
 
