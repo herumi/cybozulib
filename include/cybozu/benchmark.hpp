2d0bd6677774eb6a46bef61e6ac75ea5224fe1c7
MITSUNARI Shigeo <herumi@nifty.com>
2013-05-17 09:31:08 +0900

    emulation of CpuClock(not complete)

--- include/cybozu/benchmark.hpp
+++ include/cybozu/benchmark.hpp
@@ -5,12 +5,22 @@
 	@author MITSUNARI Shigeo
 */
 #include <cybozu/inttype.hpp>
+#include <stdio.h>
+
+#if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__)
+	#define CYBOZU_BENCH_USE_RDTSC
+#endif
+#ifdef CYBOZU_BENCH_USE_RDTSC
 #ifdef _MSC_VER
 	#include <intrin.h>
+#endif
+#else
+	#include <cybozu/time.hpp>
 #endif
 
 namespace cybozu {
 
+#ifdef CYBOZU_BENCH_USE_RDTSC
 class CpuClock {
 public:
 	static inline uint64_t getRdtsc()
@@ -40,29 +50,72 @@ public:
 	int getCount() const { return count_; }
 	uint64_t getClock() const { return clock_; }
 	void clear() { count_ = 0; clock_ = 0; }
+	void put(const char *msg = 0, int N = 1) const
+	{
+		double t = getClock() / double(getCount()) / N;
+		if (msg && *msg) printf("%s ", msg);
+		if (t > 1e6) {
+			printf("%6.2fMclk", t * 1e-6);
+		} else if (t > 1e3) {
+			printf("%6.2fKclk", t * 1e-3);
+		} else {
+			printf("%6.2f clk", t);
+		}
+		if (msg && *msg) printf("\n");
+	}
+	// adhoc constatns for CYBOZU_BENCH
+	static const int loopN1 = 1000;
+	static const int loopN2 = 1000000;
+	static const uint64_t maxClk = (uint64_t)3e8;
 private:
 	uint64_t clock_;
 	int count_;
 };
-
-namespace benchmark_local {
-
-inline void putClk(const char *msg, const cybozu::CpuClock& clk, int N)
-{
-	double t = clk.getClock() / double(clk.getCount()) / N;
-	if (msg && *msg) printf("%s ", msg);
-	if (t > 1e6) {
-		printf("%6.2f Mclk", t * 1e-6);
-	} else if (t > 1e3) {
-		printf("%6.2f Kclk", t * 1e-3);
-	} else {
-		printf("%6.2f  clk", t);
+#else
+class CpuClock {
+	cybozu::Time t_;
+	uint64_t clock_;
+	int count_;
+public:
+	CpuClock() : clock_(0), count_(0) { t_.setTime(0, 0); }
+	void begin()
+	{
+		if (count_ == 0) t_.setCurrentTime(); // start
 	}
-//	printf("(%d)", (int)clk.getCount() * N);
-	if (msg && *msg) printf("\n");
-}
-
-} // benchmark_local
+	/*
+		@note QQQ ; this is not same api as rdtsc version
+	*/
+	void end()
+	{
+		cybozu::Time cur(true);
+		int diffSec = (int)(cur.getTime() - t_.getTime());
+		int diffMsec = cur.getMsec() - t_.getMsec();
+		const int diff = diffSec * 1000 + diffMsec;
+		clock_ = diff;
+		count_++;
+	}
+	int getCount() const { return count_; }
+	uint64_t getClock() const { return clock_; }
+	void clear() { t_.setTime(0, 0); clock_ = 0; count_ = 0; }
+	void put(const char *msg = 0, int N = 1) const
+	{
+		double t = getClock() / double(getCount()) / N;
+		if (msg && *msg) printf("%s ", msg);
+		if (t > 1) {
+			printf("%6.2fmsec", t);
+		} else if (t > 1e-3) {
+			printf("%6.2fusec", t * 1e3);
+		} else {
+			printf("%6.2fnsec", t * 1e6);
+		}
+		if (msg && *msg) printf("\n");
+	}
+	// adhoc constatns for CYBOZU_BENCH
+	static const int loopN1 = 1000000;
+	static const int loopN2 = 1000;
+	static const uint64_t maxClk = (uint64_t)500;
+};
+#endif
 
 /*
 	loop counter is automatically determined
@@ -71,16 +124,16 @@ inline void putClk(const char *msg, const cybozu::CpuClock& clk, int N)
 */
 #define CYBOZU_BENCH(msg, func, ...) \
 { \
-	const uint64_t MAX_CLK = (uint64_t)3e8; \
-	const int N = 1000; \
+	const uint64_t MAX_CLK = cybozu::CpuClock::maxClk; \
+	const int N = cybozu::CpuClock::loopN1; \
 	cybozu::CpuClock clk; \
-	for (int i = 0; i < 1000000; i++) { \
+	for (int i = 0; i < cybozu::CpuClock::loopN2; i++) { \
 		clk.begin(); \
 		for (int j = 0; j < N; j++) func(__VA_ARGS__); \
 		clk.end(); \
 		if (clk.getClock() > MAX_CLK) break; \
 	} \
-	cybozu::benchmark_local::putClk(msg, clk, N); \
+	clk.put(msg, N); \
 }
 
 /*
