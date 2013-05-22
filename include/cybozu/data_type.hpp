7f16021913207c7ca9b458f87668329f5b326b8d
MITSUNARI Shigeo <herumi@nifty.com>
2013-05-22 14:58:12 +0900

    add alias of data type

--- include/cybozu/data_type.hpp
+++ include/cybozu/data_type.hpp
@@ -9,16 +9,19 @@
 #include <map>
 #include <set>
 #include <vector>
+#include <cybozu/inttype.hpp>
 
 namespace cybozu {
 
 typedef std::vector<int> IntVec;
+typedef std::vector<uint32_t> Uint32Vec;
 typedef std::vector<std::string> StrVec;
 typedef std::map<int, int> Int2Int;
 typedef std::map<int, std::string> Int2Str;
 typedef std::map<std::string, int> Str2Int;
 typedef std::map<std::string, std::string> Str2Str;
 typedef std::set<int> IntSet;
+typedef std::set<uint32_t> Uint32Set;
 typedef std::set<std::string> StrSet;
 
 } // cybozu
