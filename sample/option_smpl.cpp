bbd6984895e98d1c558d3b9e9f8db75bc6f0fb58
MITSUNARI Shigeo <herumi@nifty.com>
2013-07-08 16:50:14 +0900

    Option class

---
+++ sample/option_smpl.cpp
@@ -0,0 +1,42 @@
+/*
+	how to use
+*/
+#include <stdio.h>
+#include <cybozu/option.hpp>
+#include <vector>
+
+struct A : public cybozu::Option<A> {
+	int x;
+	bool b;
+	double d;
+	std::string y;
+	std::vector<int> z;
+	std::vector<std::string> w;
+	std::string inName;
+	std::vector<std::string> r;
+	std::vector<int> vi;
+	A()
+	{
+		appendOpt(&x, 5, "x", "int");
+		appendOpt(&b, false, "b", "bool");
+		appendMust(&d, "d", "double");
+		appendMust(&y, "y", "string");
+		appendVec(&z, "z", "int int int ...");
+		appendVec(&w, "w", "str str str ...");
+		appendParam(&inName, "input-file", "text file");
+		appendParamVec(&vi, "remains", "sss");
+	}
+};
+
+int main(int argc, char *argv[])
+	try
+{
+	A a;
+	if (a.parse(argc, argv)) {
+		a.put();
+	} else {
+		a.usage();
+	}
+} catch (std::exception& e) {
+	printf("ERR %s\n", e.what());
+}
