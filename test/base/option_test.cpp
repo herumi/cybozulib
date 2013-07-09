c8efcef366263202e93954d69e71e571d558f91e
MITSUNARI Shigeo <herumi@nifty.com>
2013-07-09 15:25:07 +0900

    add ParamOpt ; add test

--- test/base/option_test.cpp
+++ test/base/option_test.cpp
@@ -1,6 +1,8 @@
 #include <cybozu/option.hpp>
 #include <cybozu/test.hpp>
 
+char progName[] = "test";
+
 CYBOZU_TEST_AUTO(convert_int)
 {
 	struct {
@@ -172,6 +174,27 @@ CYBOZU_TEST_AUTO(Var)
 		CYBOZU_TEST_ASSERT(v.isSet());
 	}
 	{
+		bool x = false;
+		cybozu::option_local::Var v(&x);
+		CYBOZU_TEST_ASSERT(!v.isSet());
+		v.set("1");
+		CYBOZU_TEST_ASSERT(x);
+		CYBOZU_TEST_EQUAL(v.toStr(), "1");
+		CYBOZU_TEST_ASSERT(v.isSet());
+		v.set("0");
+		CYBOZU_TEST_ASSERT(!x);
+		CYBOZU_TEST_EQUAL(v.toStr(), "0");
+	}
+	{
+		char x = 0;
+		cybozu::option_local::Var v(&x);
+		CYBOZU_TEST_ASSERT(!v.isSet());
+		v.set("a");
+		CYBOZU_TEST_EQUAL(x, 'a');
+		CYBOZU_TEST_EQUAL(v.toStr(), "a");
+		CYBOZU_TEST_ASSERT(v.isSet());
+	}
+	{
 		std::vector<int> x;
 		cybozu::option_local::Var v(&x);
 		CYBOZU_TEST_ASSERT(!v.isSet());
@@ -183,4 +206,146 @@ CYBOZU_TEST_AUTO(Var)
 		CYBOZU_TEST_EQUAL(v.toStr(), "5 1234");
 		CYBOZU_TEST_ASSERT(v.isSet());
 	}
+}
+
+CYBOZU_TEST_AUTO(appendOpt)
+{
+	cybozu::Option opt;
+	int x;
+	opt.appendOpt(&x, 3, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(opt.parse(1, argv));
+		CYBOZU_TEST_EQUAL(x, 3);
+	}
+	{
+		char optX[] = "-x";
+		char i999[] = "999";
+		char *argv[] = { progName, optX, i999 };
+		CYBOZU_TEST_ASSERT(opt.parse(3, argv));
+		CYBOZU_TEST_EQUAL(x, 999);
+	}
+}
+
+CYBOZU_TEST_AUTO(appendMust)
+{
+	cybozu::Option opt;
+	int x = 9;
+	opt.appendMust(&x, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(!opt.parse(1, argv));
+	}
+	{
+		char optX[] = "-x";
+		char i999[] = "999";
+		char *argv[] = { progName, optX, i999 };
+		CYBOZU_TEST_ASSERT(opt.parse(3, argv));
+		CYBOZU_TEST_EQUAL(x, 999);
+	}
+}
+
+CYBOZU_TEST_AUTO(appendVec)
+{
+	cybozu::Option opt;
+	std::vector<int> x;
+	opt.appendVec(&x, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(opt.parse(1, argv));
+		CYBOZU_TEST_ASSERT(x.empty());
+	}
+	{
+		char optX[] = "-x";
+		char i999[] = "999";
+		char i123[] = "123";
+		char *argv[] = { progName, optX, i999, i123 };
+		CYBOZU_TEST_ASSERT(opt.parse(4, argv));
+		CYBOZU_TEST_EQUAL(x.size(), 2u);
+		CYBOZU_TEST_EQUAL(x[0], 999);
+		CYBOZU_TEST_EQUAL(x[1], 123);
+	}
+}
+
+CYBOZU_TEST_AUTO(appendParam)
+{
+	cybozu::Option opt;
+	std::string x;
+	opt.appendParam(&x, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(!opt.parse(1, argv));
+	}
+	{
+		char aaa[] = "aaa";
+		char *argv[] = { progName, aaa };
+		CYBOZU_TEST_ASSERT(opt.parse(2, argv));
+		CYBOZU_TEST_EQUAL(x, "aaa");
+	}
+}
+
+CYBOZU_TEST_AUTO(appendParamOpt)
+{
+	cybozu::Option opt;
+	std::string x;
+	opt.appendParamOpt(&x, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(opt.parse(1, argv));
+		CYBOZU_TEST_ASSERT(x.empty());
+	}
+	{
+		char aaa[] = "aaa";
+		char *argv[] = { progName, aaa };
+		CYBOZU_TEST_ASSERT(opt.parse(2, argv));
+		CYBOZU_TEST_EQUAL(x, "aaa");
+	}
+}
+
+CYBOZU_TEST_AUTO(appendParamVec)
+{
+	cybozu::Option opt;
+	std::vector<char> x;
+	opt.appendParamVec(&x, "x");
+	{
+		char *argv[] = { progName };
+		CYBOZU_TEST_ASSERT(opt.parse(1, argv));
+		CYBOZU_TEST_ASSERT(x.empty());
+	}
+	{
+		char a[] = "a";
+		char z[] = "z";
+		char *argv[] = { progName, a, z };
+		CYBOZU_TEST_ASSERT(opt.parse(3, argv));
+		CYBOZU_TEST_EQUAL(x.size(), 2u);
+		CYBOZU_TEST_EQUAL(x[0], 'a');
+		CYBOZU_TEST_EQUAL(x[1], 'z');
+	}
+}
+
+CYBOZU_TEST_AUTO(dupplicate)
+{
+	cybozu::Option opt;
+	int x;
+	opt.appendMust(&x, "x");
+	CYBOZU_TEST_EXCEPTION(opt.appendMust(&x, "x"), cybozu::Exception);
+}
+
+CYBOZU_TEST_AUTO(appendParamErr)
+{
+	cybozu::Option opt;
+	int x;
+	int y;
+	std::vector<int> z;
+	opt.appendParamOpt(&x, "x");
+	CYBOZU_TEST_EXCEPTION(opt.appendParam(&y, "y"), cybozu::Exception);
+	CYBOZU_TEST_EXCEPTION(opt.appendParamVec(&z, "z"), cybozu::Exception);
+}
+
+CYBOZU_TEST_AUTO(opt_begin_with_not_a_number)
+{
+	cybozu::Option opt;
+	int x;
+	// opt can't srat number
+	CYBOZU_TEST_EXCEPTION(opt.appendOpt(&x, 9, "3a"), cybozu::Exception);
 }
