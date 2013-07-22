07e9b8c35a013fa2a8c4d8391dd2a839dfe18fd1
MITSUNARI Shigeo <herumi@nifty.com>
2013-07-22 09:55:13 +0900

    VC does not support va_copy

--- include/cybozu/format.hpp
+++ include/cybozu/format.hpp
@@ -16,15 +16,13 @@ namespace cybozu {
 inline void vformat(std::string& str, const char *format, va_list args)
 {
 #ifdef _MSC_VER
-	va_list args2;
-	va_copy(args2, args);
 	_locale_t curLoc = _get_current_locale();
 	int size = _vscprintf_l(format, curLoc, args);
 	if (size < 0 || size >= INT_MAX) throw cybozu::Exception("vformat:_vscprintf_l");
 
 	str.resize(size + 1);
 
-	int ret = _vsprintf_s_l(&str[0], size + 1, format, curLoc, args2);
+	int ret = _vsprintf_s_l(&str[0], size + 1, format, curLoc, args);
 	if (ret < 0) throw cybozu::Exception("vformat:_vsprintf_s_l");
 	str.resize(size);
 #else
