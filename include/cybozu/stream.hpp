736ae86e3097f68464f4b39f44ddd52b2c5108ad
MITSUNARI Shigeo <herumi@nifty.com>
2013-07-26 11:49:39 +0900

    new api of stream ; void read(), void write(), size_t readSome()

--- include/cybozu/stream.hpp
+++ include/cybozu/stream.hpp
@@ -30,17 +30,48 @@ struct enable_if { typedef T type; };
 template <class T>
 struct enable_if<false, T> {};
 
+/* specialization for istream */
 template<class InputStream>
-size_t readSub(InputStream& is, char *buf, size_t size, typename enable_if<is_convertible<InputStream, std::istream>::value>::type* = 0)
+size_t readSome_inner(InputStream& is, void *buf, size_t size, typename enable_if<is_convertible<InputStream, std::istream>::value>::type* = 0)
 {
-	is.read(buf, size);
-	return (size_t)is.gcount();
+	is.read((char *)buf, size);
+	const std::streamsize readSize = is.gcount();
+	if (readSize < 0) throw cybozu::Exception("stream:readSome_inner:bad readSize") << size << readSize;
+	return readSize;
 }
 
 template<class InputStream>
-size_t readSub(InputStream& is, char *buf, size_t size, typename enable_if<!is_convertible<InputStream, std::istream>::value>::type* = 0)
+void read_inner(InputStream& is, void *buf, size_t size, typename enable_if<is_convertible<InputStream, std::istream>::value>::type* = 0)
 {
-	return is.read(buf, size);
+	if (!is.read((char *)buf, size) || size != (size_t)is.gcount()) throw cybozu::Exception("stream:read_inner") << size;
+}
+
+/* specialization for ostream */
+template<class OutputStream>
+void writeSub(OutputStream& os, const void *buf, size_t size, typename enable_if<is_convertible<OutputStream, std::ostream>::value>::type* = 0)
+{
+	if (!os.write((const char *)buf, size)) throw cybozu::Exception("stream:writeSub") << size;
+}
+
+/* generic version for size_t readSome(void *, size_t) */
+template<class InputStream>
+size_t readSome_inner(InputStream& is, void *buf, size_t size, typename enable_if<!is_convertible<InputStream, std::istream>::value>::type* = 0)
+{
+	return is.readSome(buf, size);
+}
+
+/* generic version for void read(void *, size_t), which reads all data */
+template<class InputStream>
+void read_inner(InputStream& is, void *buf, size_t size, typename enable_if<!is_convertible<InputStream, std::istream>::value>::type* = 0)
+{
+	is.read(buf, size);
+}
+
+/* generic version for void write(const void*, size_t), which writes all data */
+template<class OutputStream>
+void writeSub(OutputStream& os, const void *buf, size_t size, typename enable_if<!is_convertible<OutputStream, std::ostream>::value>::type* = 0)
+{
+	os.write(buf, size);
 }
 
 } // stream_local
@@ -50,17 +81,21 @@ size_t readSub(InputStream& is, char *buf, size_t size, typename enable_if<!is_c
 */
 template<class InputStream>
 struct InputStreamTag {
-	static inline size_t read(InputStream& is, char *buf, size_t size)
+	static size_t readSome(InputStream& is, void *buf, size_t size)
 	{
-		return stream_local::readSub<InputStream>(is, buf, size);
+		return stream_local::readSome_inner<InputStream>(is, buf, size);
+	}
+	static inline void read(InputStream& is, void *buf, size_t size)
+	{
+		stream_local::read_inner<InputStream>(is, buf, size);
 	}
 };
 
 template<class OutputStream>
 struct OutputStreamTag {
-	static inline void write(OutputStream& os, const char *buf, size_t size)
+	static inline void write(OutputStream& os, const void *buf, size_t size)
 	{
-		if (!os.write(buf, size)) throw cybozu::Exception("OutputStream:write") << size;
+		stream_local::writeSub<OutputStream>(os, buf, size);
 	}
 };
 
@@ -68,8 +103,8 @@ class MemoryInputStream {
 	const char *p_;
 	size_t size_;
 public:
-	MemoryInputStream(const char *p, size_t size) : p_(p), size_(size) {}
-	size_t read(char *buf, size_t size, const char * = "")
+	MemoryInputStream(const void *p, size_t size) : p_((const char *)p), size_(size) {}
+	size_t readSome(void *buf, size_t size)
 	{
 		if (size > size_) size = size_;
 		memcpy(buf, p_, size);
@@ -77,20 +112,23 @@ public:
 		size_ -= size;
 		return size;
 	}
+	void read(void *buf, size_t size)
+	{
+		if (readSome(buf, size) != size) throw cybozu::Exception("MemoryInputStream:read") << size;
+	}
 };
 
 class MemoryOutputStream {
 	char *p_;
 	size_t size_;
 public:
-	MemoryOutputStream(char *p, size_t size) : p_(p), size_(size) {}
-	bool write(const char *buf, size_t size)
+	MemoryOutputStream(void *p, size_t size) : p_((char *)p), size_(size) {}
+	void write(const void *buf, size_t size)
 	{
-		if (size > size_) return false;
+		if (size > size_) throw cybozu::Exception("MemoryOutputStream:write") << size << size_;
 		memcpy(p_, buf, size);
 		p_ += size;
 		size_ -= size;
-		return true;
 	}
 };
 
@@ -101,13 +139,17 @@ class StringInputStream {
 	void operator=(const StringInputStream&);
 public:
 	explicit StringInputStream(const std::string& str) : str_(str), cur_(0) {}
-	size_t read(char *buf, size_t size)
+	size_t readSome(void *buf, size_t size)
 	{
 		const size_t remainSize = str_.size() - cur_;
 		if (size > remainSize) size = remainSize;
 		memcpy(buf, &str_[cur_], size);
 		cur_ += size;
 		return size;
+	}
+	void read(void *buf, size_t size)
+	{
+		if (readSome(buf, size) != size) throw cybozu::Exception("StringInputStream:read") << size;
 	}
 };
 
@@ -117,10 +159,9 @@ class StringOutputStream {
 	void operator=(const StringOutputStream&);
 public:
 	explicit StringOutputStream(std::string& str) : str_(str) {}
-	bool write(const char *buf, size_t size)
+	void write(const void *buf, size_t size)
 	{
-		str_.append(buf, size);
-		return true;
+		str_.append((const char *)buf, size);
 	}
 };
 
