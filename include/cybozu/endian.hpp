e719d6523cf2fc1b08ea78749cd1775ab9bcdcb6
MITSUNARI Shigeo <herumi@nifty.com>
2013-10-16 14:07:08 +0900

    remove old-style cast in endian

--- include/cybozu/endian.hpp
+++ include/cybozu/endian.hpp
@@ -27,7 +27,7 @@ inline uint16_t Get16bitAsLE(const void *src)
 inline uint32_t Get32bitAsLE(const void *src)
 {
 	const uint8_t *p = static_cast<const uint8_t *>(src);
-	return Get16bitAsLE(p) | ((uint32_t)Get16bitAsLE(p + 2) << 16);
+	return Get16bitAsLE(p) | (static_cast<uint32_t>(Get16bitAsLE(p + 2)) << 16);
 }
 
 /**
@@ -57,7 +57,7 @@ inline uint16_t Get16bitAsBE(const void *src)
 inline uint32_t Get32bitAsBE(const void *src)
 {
 	const uint8_t *p = static_cast<const uint8_t *>(src);
-	return Get16bitAsBE(p + 2) | ((uint32_t)Get16bitAsBE(p) << 16);
+	return Get16bitAsBE(p + 2) | (static_cast<uint32_t>(Get16bitAsBE(p)) << 16);
 }
 
 /**
@@ -89,10 +89,10 @@ inline void Set16bitAsLE(void *src, uint16_t x)
 inline void Set32bitAsLE(void *src, uint32_t x)
 {
 	uint8_t *p = static_cast<uint8_t *>(src);
-	p[0] = (uint8_t)x;
-	p[1] = (uint8_t)(x >> 8);
-	p[2] = (uint8_t)(x >> 16);
-	p[3] = (uint8_t)(x >> 24);
+	p[0] = static_cast<uint8_t>(x);
+	p[1] = static_cast<uint8_t>(x >> 8);
+	p[2] = static_cast<uint8_t>(x >> 16);
+	p[3] = static_cast<uint8_t>(x >> 24);
 }
 /**
 	set 64bit integer as little endian
@@ -102,8 +102,8 @@ inline void Set32bitAsLE(void *src, uint32_t x)
 inline void Set64bitAsLE(void *src, uint64_t x)
 {
 	uint8_t *p = static_cast<uint8_t *>(src);
-	Set32bitAsLE(p, (uint32_t)x);
-	Set32bitAsLE(p + 4, (uint32_t)(x >> 32));
+	Set32bitAsLE(p, static_cast<uint32_t>(x));
+	Set32bitAsLE(p + 4, static_cast<uint32_t>(x >> 32));
 }
 /**
 	set 16bit integer as big endian
@@ -113,8 +113,8 @@ inline void Set64bitAsLE(void *src, uint64_t x)
 inline void Set16bitAsBE(void *src, uint16_t x)
 {
 	uint8_t *p = static_cast<uint8_t *>(src);
-	p[0] = (uint8_t)(x >> 8);
-	p[1] = (uint8_t)x;
+	p[0] = static_cast<uint8_t>(x >> 8);
+	p[1] = static_cast<uint8_t>(x);
 }
 /**
 	set 32bit integer as big endian
@@ -124,10 +124,10 @@ inline void Set16bitAsBE(void *src, uint16_t x)
 inline void Set32bitAsBE(void *src, uint32_t x)
 {
 	uint8_t *p = static_cast<uint8_t *>(src);
-	p[0] = (uint8_t)(x >> 24);
-	p[1] = (uint8_t)(x >> 16);
-	p[2] = (uint8_t)(x >> 8);
-	p[3] = (uint8_t)x;
+	p[0] = static_cast<uint8_t>(x >> 24);
+	p[1] = static_cast<uint8_t>(x >> 16);
+	p[2] = static_cast<uint8_t>(x >> 8);
+	p[3] = static_cast<uint8_t>(x);
 }
 /**
 	set 64bit integer as big endian
@@ -137,8 +137,8 @@ inline void Set32bitAsBE(void *src, uint32_t x)
 inline void Set64bitAsBE(void *src, uint64_t x)
 {
 	uint8_t *p = static_cast<uint8_t *>(src);
-	Set32bitAsBE(p, (uint32_t)(x >> 32));
-	Set32bitAsBE(p + 4, (uint32_t)x);
+	Set32bitAsBE(p, static_cast<uint32_t>(x >> 32));
+	Set32bitAsBE(p + 4, static_cast<uint32_t>(x));
 }
 
 } // cybozu
