cdc8820309b5aa07721418943dee263be64494ca
MITSUNARI Shigeo <herumi@nifty.com>
2013-10-16 14:16:02 +0900

    remove old-style cast in stream_fwd

--- include/cybozu/stream_fwd.hpp
+++ include/cybozu/stream_fwd.hpp
@@ -20,7 +20,7 @@ namespace stream {
 template<typename InputStream>
 void read(InputStream& is, void *buf, size_t size)
 {
-	char *p = (char*)buf;
+	char *p = static_cast<char*>(buf);
 	while (size > 0) {
 		size_t readSize = cybozu::InputStreamTag<InputStream>::readSome(is, p, size);
 		if (readSize == 0) throw cybozu::Exception("stream:InputStreamRead");
