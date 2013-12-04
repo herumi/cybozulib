7d80b6f2157be1b0d6e036a0de77e6b829aa84ef
MITSUNARI Shigeo <herumi@nifty.com>
2013-12-04 14:21:03 +0900

    type of threadNum is size_t

--- include/cybozu/parallel.hpp
+++ include/cybozu/parallel.hpp
@@ -19,7 +19,7 @@ struct Thread : public cybozu::ThreadBase {
 	T *t_;
 	N begin_;
 	N end_;
-	int threadIdx_;
+	size_t threadIdx_;
 	std::string err_;
 	Thread()
 		: t_(0)
@@ -28,7 +28,7 @@ struct Thread : public cybozu::ThreadBase {
 		, threadIdx_(0)
 	{
 	}
-	void init(T& t, N begin, N end, int threadIdx)
+	void init(T& t, N begin, N end, size_t threadIdx)
 	{
 		t_ = &t;
 		begin_ = begin;
@@ -52,32 +52,32 @@ struct Thread : public cybozu::ThreadBase {
 
 } // parallel_util
 /*
-	void T::f(N i, int threadIdx);
+	void T::f(N i, size_t threadIdx);
 */
 template<class T, class N>
-void parallel_for(T& target, N n, int threadNum)
+void parallel_for(T& target, N n, size_t threadNum)
 {
-	if (threadNum <= 0) throw cybozu::Exception("cybozu:parallel_for:bad threadNum") << threadNum;
-	if ((N)threadNum > n) threadNum = (int)n;
+	if (threadNum == 0) throw cybozu::Exception("cybozu:parallel_for:threadNum is zero");
+	if ((N)threadNum > n) threadNum = (size_t)n;
 	typedef parallel_util::Thread<T, N> Thread;
 	{
 		cybozu::ScopedArray<Thread> thread(threadNum);
 		const N q = N(n / threadNum);
 		const N r = N(n % threadNum);
 		N begin = 0;
-		for (int i = 0; i < threadNum; i++) {
+		for (size_t i = 0; i < threadNum; i++) {
 			N end = begin + q;
 			if (i < r) end++;
 			thread[i].init(target, begin, end, i);
 			begin = end;
 		}
-		for (int i = 0; i < threadNum; i++) {
+		for (size_t i = 0; i < threadNum; i++) {
 			if (!thread[i].beginThread()) throw cybozu::Exception("cybozu:parallel_for:can't beginThread") << i;
 		}
-		for (int i = 0; i < threadNum; i++) {
+		for (size_t i = 0; i < threadNum; i++) {
 			if (!thread[i].joinThread()) throw cybozu::Exception("cybozu:parallel_for:can't joinThread") << i;
 		}
-		for (int i = 0; i < threadNum; i++) {
+		for (size_t i = 0; i < threadNum; i++) {
 			const std::string& err = thread[i].getErr();
 			if (!err.empty()) throw cybozu::Exception("cybozu:paralell_for") << i << err;
 		}
