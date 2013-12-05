ca33269de42b15c70cefc80066e8d3459d22ceb3
MITSUNARI Shigeo <herumi@nifty.com>
2013-12-05 11:08:35 +0900

    builtin_bswap16 is missing

--- include/cybozu/endian.hpp
+++ include/cybozu/endian.hpp
@@ -18,7 +18,7 @@ inline uint16_t byteSwap(uint16_t x) { return _byteswap_ushort(x); }
 inline uint32_t byteSwap(uint32_t x) { return _byteswap_ulong(x); }
 inline uint64_t byteSwap(uint64_t x) { return _byteswap_uint64(x); }
 #else
-inline uint16_t byteSwap(uint16_t x) { return __builtin_bswap16(x); }
+inline uint16_t byteSwap(uint16_t x) { return (x >> 8) | (x << 8); }
 inline uint32_t byteSwap(uint32_t x) { return __builtin_bswap32(x); }
 inline uint64_t byteSwap(uint64_t x) { return __builtin_bswap64(x); }
 #endif
