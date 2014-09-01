ece6837a0e743f86946d18b04fc06f1c7f6b27fe
MITSUNARI Shigeo <herumi@nifty.com>
2014-09-02 08:12:22 +0900

    BitVector::apend

--- include/cybozu/bitvector.hpp
+++ include/cybozu/bitvector.hpp
@@ -9,40 +9,175 @@
 #include <cybozu/exception.hpp>
 #include <algorithm>
 #include <vector>
+#include <assert.h>
 
 namespace cybozu {
 
 template<class T>
+size_t RoundupBit(size_t bitLen)
+{
+	const size_t unitSize = sizeof(T) * 8;
+	return (bitLen + unitSize - 1) / unitSize;
+}
+
+template<class T>
+T GetMaskBit(size_t bitLen)
+{
+	assert(bitLen < sizeof(T) * 8);
+	return (T(1) << bitLen) - 1;
+}
+
+template<class T>
+void SetBlockBit(T *buf, size_t bitLen)
+{
+	const size_t unitSize = sizeof(T) * 8;
+	const size_t q = bitLen / unitSize;
+	const size_t r = bitLen % unitSize;
+	buf[q] |= T(1) << r;
+}
+template<class T>
+void ResetBlockBit(T *buf, size_t bitLen)
+{
+	const size_t unitSize = sizeof(T) * 8;
+	const size_t q = bitLen / unitSize;
+	const size_t r = bitLen % unitSize;
+	buf[q] &= ~(T(1) << r);
+}
+template<class T>
+bool GetBlockBit(const T *buf, size_t bitLen)
+{
+	const size_t unitSize = sizeof(T) * 8;
+	const size_t q = bitLen / unitSize;
+	const size_t r = bitLen % unitSize;
+	return (buf[q] & (T(1) << r)) != 0;
+}
+
+template<class T>
+void CopyBit(T* dst, const T* src, size_t bitLen)
+{
+	const size_t unitSize = sizeof(T) * 8;
+	const size_t q = bitLen / unitSize;
+	const size_t r = bitLen % unitSize;
+	for (size_t i = 0; i < q; i++) dst[i] = src[i];
+	if (r == 0) return;
+	dst[q] = src[q] & GetMaskBit<T>(r);
+}
+/*
+	dst[] = (src[] << shift) | ext
+	@param dst [out] dst[0..n)
+	@param src [in] src[0..n)
+	@param bitLen [in] length of src, dst
+	@param shift [in] 0 <= shift < unitSize
+	@param ext [in] or bit
+*/
+template<class T>
+T ShiftLeftBit(T* dst, const T* src, size_t bitLen, size_t shift, T ext = 0)
+{
+	if (bitLen == 0) return 0;
+	const size_t unitSize = sizeof(T) * 8;
+	if (shift >= unitSize) {
+		throw cybozu::Exception("ShiftLeftBit:large shift") << shift;
+	}
+	const size_t n = RoundupBit<T>(bitLen); // n >= 1 because bitLen > 0
+	const size_t r = bitLen % unitSize;
+	const T mask = r > 0 ? GetMaskBit<T>(r) : T(-1);
+	if (shift == 0) {
+		if (n == 1) {
+			dst[0] = (src[0] & mask) | ext;
+		} else {
+			dst[n - 1] = src[n - 1] & mask;
+			for (size_t i = n - 2; i > 0; i--) {
+				dst[i] = src[i];
+			}
+			dst[0] = src[0] | ext;
+		}
+		return 0;
+	}
+	const size_t revShift = unitSize - shift;
+	T prev = src[n - 1] & mask;
+	const T ret = prev >> revShift;
+	for (size_t i = n - 1; i > 0; i--) {
+		T v = src[i - 1];
+		dst[i] = (prev << shift) | (v >> revShift);
+		prev = v;
+	}
+	dst[0] = (prev << shift) | ext;
+	return ret;
+}
+/*
+	dst[] = src[] >> shift
+	@param dst [out] dst[0..n)
+	@param src [in] src[0..n)
+	@param bitLen [in] length of src, dst
+	@param shift [in] 0 <= shift < (sizeof(T) * 8)
+*/
+template<class T>
+void ShiftRightBit(T* dst, const T* src, size_t bitLen, size_t shift)
+{
+	if (bitLen == 0) return;
+	const size_t unitSize = sizeof(T) * 8;
+	if (shift >= unitSize) {
+		throw cybozu::Exception("ShiftRightBit:bad shift") << shift;
+	}
+	if (shift == 0) {
+		CopyBit<T>(dst, src, bitLen);
+		return;
+	}
+	const size_t n = RoundupBit<T>(bitLen); // n >= 1 because bitLen > 0
+	const size_t r = bitLen % unitSize;
+	const T mask = r ? GetMaskBit<T>(r) : T(-1);
+	const size_t revShift = unitSize - shift;
+	if (n == 1) {
+		dst[0] = (src[0] & mask) >> shift;
+		return;
+	}
+	T prev = src[0];
+	for (size_t i = 0; i < n - 2; i++) {
+		T v = src[i + 1];
+		dst[i] = (prev >> shift) | (v << revShift);
+		prev = v;
+	}
+	{ // i = n - 1
+		T v = src[n - 1] & mask;
+		dst[n - 2] = (prev >> shift) | (v << revShift);
+		prev = v;
+	}
+	dst[n - 1] = prev >> shift;
+}
+
+template<class T>
 class BitVectorT {
 	static const size_t unitSize = sizeof(T) * 8;
-	size_t bitSize_;
+	size_t bitLen_;
 	std::vector<T> v_;
 public:
-	BitVectorT() : bitSize_(0) {}
-	BitVectorT(const T *buf, size_t bitSize)
+	BitVectorT() : bitLen_(0) {}
+	BitVectorT(const T *buf, size_t bitLen)
 	{
-		init(buf, bitSize);
+		init(buf, bitLen);
 	}
-	void init(const T *buf, size_t bitSize)
+	void init(const T *buf, size_t bitLen)
 	{
-		resize(bitSize);
+		resize(bitLen);
 		std::copy(buf, buf + v_.size(), &v_[0]);
 	}
-	void resize(size_t bitSize)
+	void resize(size_t bitLen)
 	{
-		bitSize_ = bitSize;
-		v_.resize((bitSize + unitSize - 1) / unitSize);
+		bitLen_ = bitLen;
+		v_.resize(RoundupBit<T>(bitLen));
+	}
+	void reserve(size_t bitLen)
+	{
+		v_.reserve(RoundupBit<T>(bitLen));
 	}
 	bool get(size_t idx) const
 	{
-		if (idx >= bitSize_) throw cybozu::Exception("BitVectorT:get:bad idx") << idx;
-		size_t q = idx / unitSize;
-		size_t r = idx % unitSize;
-		return (v_[q] & (T(1) << r)) != 0;
+		if (idx >= bitLen_) throw cybozu::Exception("BitVectorT:get:bad idx") << idx;
+		return GetBlockBit(v_.data(), idx);
 	}
 	void clear()
 	{
-		bitSize_ = 0;
+		bitLen_ = 0;
 		v_.clear();
 	}
 	void set(size_t idx, bool b)
@@ -56,23 +191,37 @@ public:
 	// set(idx, true);
 	void set(size_t idx)
 	{
-		if (idx >= bitSize_) throw cybozu::Exception("BitVectorT:set:bad idx") << idx;
-		size_t q = idx / unitSize;
-		size_t r = idx % unitSize;
-		v_[q] |= T(1) << r;
+		if (idx >= bitLen_) throw cybozu::Exception("BitVectorT:set:bad idx") << idx;
+		SetBlockBit(v_.data(), idx);
 	}
 	// set(idx, false);
 	void reset(size_t idx)
 	{
-		if (idx >= bitSize_) throw cybozu::Exception("BitVectorT:reset:bad idx") << idx;
-		size_t q = idx / unitSize;
-		size_t r = idx % unitSize;
-		v_[q] &= ~(T(1) << r);
+		if (idx >= bitLen_) throw cybozu::Exception("BitVectorT:reset:bad idx") << idx;
+		ResetBlockBit(v_.data(), idx);
 	}
-	size_t size() const { return bitSize_; }
+	size_t size() const { return bitLen_; }
 	const T *getBlock() const { return &v_[0]; }
 	T *getBlock() { return &v_[0]; }
 	size_t getBlockSize() const { return v_.size(); }
+	/*
+		append src[0, bitLen)
+	*/
+	void append(const T* src, size_t bitLen)
+	{
+		if (bitLen == 0) return;
+		const size_t sq = bitLen_ / unitSize;
+		const size_t sr = bitLen_ % unitSize;
+		resize(bitLen_ + bitLen);
+		if (sr == 0) {
+			CopyBit<T>(&v_[sq], src, bitLen);
+			return;
+		}
+		T over = ShiftLeftBit<T>(&v_[sq], src, bitLen, sr, v_[sq] & GetMaskBit<T>(sr));
+		if (RoundupBit<T>(bitLen + sr) > RoundupBit<T>(bitLen)) {
+			v_[v_.size() - 1] = over;
+		}
+	}
 };
 
 typedef BitVectorT<size_t> BitVector;
--- test/base/bitvector_test.cpp
+++ test/base/bitvector_test.cpp
@@ -21,3 +21,75 @@ CYBOZU_TEST_AUTO(set)
 		}
 	}
 }
+
+struct StdVec : std::vector<bool> {
+	template<class T>
+	void append(const T*src, size_t bitLen)
+	{
+		for (size_t i = 0; i < bitLen; i++) {
+			bool b = cybozu::GetBlockBit(src, i);
+			push_back(b);
+		}
+	}
+};
+
+template<class T>
+void verifyVec(const T& v1, const StdVec& v2)
+{
+	CYBOZU_TEST_EQUAL(v1.size(), v2.size());
+	for (size_t i = 0; i < v1.size(); i++) {
+		CYBOZU_TEST_EQUAL(v1.get(i), v2[i]);
+	}
+}
+
+CYBOZU_TEST_AUTO(ShiftRightBit)
+{
+	const struct {
+		uint32_t x[4];
+		size_t bitLen;
+		size_t shift;
+		uint32_t z[4];
+	} tbl[] = {
+		{ { 0x12345678, 0, 0, 0 }, 128, 0, { 0x12345678, 0, 0, 0 } },
+		{ { 0x12345678, 0, 0, 0 }, 1, 0, { 0, 0, 0, 0 } },
+		{ { 0x12345678, 0, 0, 0 }, 10, 0, { 0x278, 0, 0, 0 } },
+		{ { 0x12345678, 0, 0, 0 }, 1, 1, { 0, 0, 0, 0 } },
+		{ { 0x12345678, 0, 0, 0 }, 13, 5, { 0xb3, 0, 0, 0 } },
+		{ { 0x12345678, 0xaaaabbbb, 0xffeebbcc, 0xfeba9874 }, 128, 1, { 0x891a2b3c, 0x55555ddd, 0x7ff75de6, 0x7f5d4c3a } },
+		{ { 0x12345678, 0xaaaabbbb, 0xffeebbcc, 0xfeba9874 }, 128, 18, { 0xaeeec48d, 0xaef32aaa, 0xa61d3ffb, 0x3fae } },
+		{ { 0x12345678, 0xaaaabbbb, 0xffeebbcc, 0xfeba9874 }, 97, 18, { 0xaeeec48d, 0xaef32aaa, 0x3ffb, 0 } },
+	};
+	for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
+		uint32_t z[4];
+		cybozu::ShiftRightBit(z, tbl[i].x, tbl[i].bitLen, tbl[i].shift);
+		const uint32_t n = cybozu::RoundupBit<uint32_t>(tbl[i].bitLen);
+		CYBOZU_TEST_EQUAL_ARRAY(z, tbl[i].z, n);
+	}
+}
+
+CYBOZU_TEST_AUTO(append)
+{
+	const uint16_t src1[] = { 0x3210, 0x7654, 0xba98, 0xfedc };
+	const uint16_t src2[] = { 0xabcd, 0xfebd, 0xffff, 0x5224 };
+	typedef cybozu::BitVectorT<uint16_t> Vec;
+	{
+		Vec v1;
+		StdVec v2;
+		v1.append(src1, 2);
+		v2.append(src1, 2);
+		v1.append(src2, 16);
+		v2.append(src2, 16);
+		verifyVec(v1, v2);
+	}
+	for (size_t i = 0; i < 48; i++) {
+		for (size_t j = 0; j < 48; j++) {
+			Vec v1;
+			StdVec v2;
+			v1.append(src1, i);
+			v2.append(src1, i);
+			v1.append(src2, j);
+			v2.append(src2, j);
+			verifyVec(v1, v2);
+		}
+	}
+}
