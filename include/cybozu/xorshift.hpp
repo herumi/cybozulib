a98d9443ddf4b8ebf09135790844aca5b6e74e0b
MITSUNARI Shigeo <herumi@nifty.com>
2016-08-03 15:00:22 +0900

    add Xoroshifto128Plus

--- include/cybozu/xorshift.hpp
+++ include/cybozu/xorshift.hpp
@@ -79,7 +79,7 @@ public:
 	{
 		return static_cast<uint32_t>(get64());
 	}
-	uint32_t operator()() { return get32(); }
+	uint64_t operator()() { return get64(); }
 	uint64_t get64()
 	{
 		uint64_t s1 = s_[0];
@@ -88,6 +88,63 @@ public:
 		s1 ^= s1 << 23;
 		s_[1] = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
 		return s_[1] + s0;
+	}
+	template<class T>
+	void read(T *x, size_t n)
+	{
+		const size_t size = sizeof(T) * n;
+		uint8_t *p8 = static_cast<uint8_t*>(x);
+		for (size_t i = 0; i < size; i++) {
+			p8[i] = static_cast<uint8_t>(get32());
+		}
+	}
+	void read(uint32_t *x, size_t n)
+	{
+		for (size_t i = 0; i < n; i++) {
+			x[i] = get32();
+		}
+	}
+	void read(uint64_t *x, size_t n)
+	{
+		for (size_t i = 0; i < n; i++) {
+			x[i] = get64();
+		}
+	}
+};
+
+// see http://xoroshiro.di.unimi.it/xoroshiro128plus.c
+class Xoroshiro128Plus {
+	uint64_t s_[2];
+	static const uint64_t seed0 = 123456789;
+	static const uint64_t seed1 = 987654321;
+	uint64_t rotl(uint64_t x, unsigned int k) const
+	{
+		return (x << k) | (x >> (64 - k));
+	}
+public:
+	explicit Xoroshiro128Plus(uint64_t s0 = seed0, uint64_t s1 = seed1)
+	{
+		init(s0, s1);
+	}
+	void init(uint64_t s0 = seed0, uint64_t s1 = seed1)
+	{
+		s_[0] = s0;
+		s_[1] = s1;
+	}
+	uint32_t get32()
+	{
+		return static_cast<uint32_t>(get64());
+	}
+	uint64_t operator()() { return get64(); }
+	uint64_t get64()
+	{
+		uint64_t s0 = s_[0];
+		uint64_t s1 = s_[1];
+		uint64_t result = s0 + s1;
+		s1 ^= s0;
+		s_[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14);
+		s_[1] = rotl(s1, 36);
+		return result;
 	}
 	template<class T>
 	void read(T *x, size_t n)
