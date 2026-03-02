842a4b7533eafbc3dca2d94cf96ad67482479db1
MITSUNARI Shigeo <herumi@nifty.com>
2026-03-02 17:57:12 +0900

    change env to check

--- test/base/env_test.cpp
+++ test/base/env_test.cpp
@@ -7,16 +7,16 @@ CYBOZU_TEST_AUTO(env)
 	std::string val;
 	CYBOZU_TEST_ASSERT(!cybozu::QueryEnv(val, "aw3vraw3vasv"));
 #ifdef _WIN32
-	const char *key = "SystemDrive";
-	const char *assumeVal = "C:";
+	const char *key = "USERPROFILE";
+	const char *assumeVal = "C:\\Users";
 #elif defined(__APPLE__)
-	const char *key = "OSTYPE";
-	const char *assumeVal = "darwin";
+	const char *key = "HOME";
+	const char *assumeVal = "/Users";
 #else
-	const char *key = "OSTYPE";
-	const char *assumeVal = "linux";
+	const char *key = "HOME";
+	const char *assumeVal = "/home";
 #endif
 	val = cybozu::GetEnv(key);
-	CYBOZU_TEST_EQUAL(val, assumeVal);
+	CYBOZU_TEST_EQUAL(val.substr(0, strlen(assumeVal)), assumeVal);
 	CYBOZU_TEST_EQUAL(cybozu::GetEnv("asvrifansevjase", "default"), "default");
 }
