46abeb6b6e652257b2797bf6fff42efc2fb87ed6
MITSUNARI Shigeo <herumi@nifty.com>
2026-08-13 09:03:07 +0900

    backend by same macros as sha2.hpp

--- include/cybozu/sha1.hpp
+++ include/cybozu/sha1.hpp
@@ -2,14 +2,138 @@
 /**
 	@file
 	@brief SHA1 class
-
 	@author MITSUNARI Shigeo(@herumi)
-	@note this is naive implementation so is not fast.
+	@license modified new BSD license
+	http://opensource.org/licenses/BSD-3-Clause
+	@note SHA-1 is deprecated ; use SHA-256 or SHA-512 in cybozu/sha2.hpp
 */
+/*
+	the backend (Apple CommonCrypto / Windows CNG(bcrypt) / OpenSSL / built-in)
+	is selected by the same macros as cybozu/sha2.hpp
+	(CYBOZU_USE_APPLE_COMMONCRYPTO, CYBOZU_USE_WIN_BCRYPT, CYBOZU_DONT_USE_OPENSSL)
+*/
+#include <cybozu/sha2.hpp>
 
-#include <cybozu/inttype.hpp>
+#if CYBOZU_USE_APPLE_COMMONCRYPTO == 1
+
+#ifdef __APPLE__
+	#pragma GCC diagnostic push
+	#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
+namespace cybozu {
+
+class Sha1 {
+	CC_SHA1_CTX ctx_;
+	/*
+		CC_SHA1_Update takes CC_LONG(=uint32_t) as a size,
+		so a larger buffer is split.
+	*/
+	static const size_t maxUpdateSize_ = size_t(1) << 30;
+public:
+	Sha1()
+	{
+		clear();
+	}
+	void clear()
+	{
+		CC_SHA1_Init(&ctx_);
+	}
+	void update(const void *buf, size_t bufSize)
+	{
+		const char *p = reinterpret_cast<const char*>(buf);
+		while (bufSize > maxUpdateSize_) {
+			CC_SHA1_Update(&ctx_, p, CC_LONG(maxUpdateSize_));
+			p += maxUpdateSize_;
+			bufSize -= maxUpdateSize_;
+		}
+		CC_SHA1_Update(&ctx_, p, CC_LONG(bufSize));
+	}
+	size_t digest(void *md, size_t mdSize, const void *buf, size_t bufSize)
+	{
+		if (mdSize < CC_SHA1_DIGEST_LENGTH) return 0;
+		update(buf, bufSize);
+		CC_SHA1_Final(reinterpret_cast<unsigned char*>(md), &ctx_);
+		return CC_SHA1_DIGEST_LENGTH;
+	}
+};
+
+} // cybozu
+
+#ifdef __APPLE__
+	#pragma GCC diagnostic pop
+#endif
+
+#elif defined(CYBOZU_USE_BCRYPT_SHA)
+
+namespace cybozu {
+
+class Sha1 {
+	sha2_local::BCryptHash<20> ctx_;
+public:
+	Sha1() : ctx_(BCRYPT_SHA1_ALGORITHM) {}
+	void clear() { ctx_.clear(); }
+	void update(const void *buf, size_t bufSize) { ctx_.update(buf, bufSize); }
+	size_t digest(void *md, size_t mdSize, const void *buf, size_t bufSize)
+	{
+		return ctx_.digest(md, mdSize, buf, bufSize);
+	}
+};
+
+} // cybozu
+
+#elif defined(CYBOZU_USE_OPENSSL_SHA)
+
+namespace cybozu {
+
+class Sha1 {
+#if CYBOZU_USE_OPENSSL_NEW_HASH == 1
+	local::NewHash<20> ctx_;
+#else
+	SHA_CTX ctx_;
+#endif
+public:
+	Sha1()
+#if CYBOZU_USE_OPENSSL_NEW_HASH == 1
+		: ctx_("sha1")
+#endif
+	{
+		clear();
+	}
+	void clear()
+	{
+#if CYBOZU_USE_OPENSSL_NEW_HASH == 1
+		ctx_.clear();
+#else
+		SHA1_Init(&ctx_);
+#endif
+	}
+	void update(const void *buf, size_t bufSize)
+	{
+#if CYBOZU_USE_OPENSSL_NEW_HASH == 1
+		ctx_.update(buf, bufSize);
+#else
+		SHA1_Update(&ctx_, buf, bufSize);
+#endif
+	}
+	size_t digest(void *md, size_t mdSize, const void *buf, size_t bufSize)
+	{
+#if CYBOZU_USE_OPENSSL_NEW_HASH == 1
+		return ctx_.digest(md, mdSize, buf, bufSize);
+#else
+		if (mdSize < SHA_DIGEST_LENGTH) return 0;
+		update(buf, bufSize);
+		SHA1_Final(reinterpret_cast<uint8_t*>(md), &ctx_);
+		return SHA_DIGEST_LENGTH;
+#endif
+	}
+};
+
+} // cybozu
+
+#else
+
 #include <cybozu/endian.hpp>
-#include <string>
 #include <algorithm>
 #include <memory.h>
 #include <assert.h>
@@ -53,25 +177,6 @@ private:
 		}
 	}
 
-	void clear()
-	{
-		static const uint32_t tbl[] = {
-			0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6
-		};
-		for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
-			for (int j = 0; j < 20; j++) {
-				K_[i * 20 + j] = tbl[i];
-			}
-		}
-		totalSize_ = 0;
-		roundBufSize_ = 0;
-		H_[0] = 0x67452301;
-		H_[1] = 0xefcdab89;
-		H_[2] = 0x98badcfe;
-		H_[3] = 0x10325476;
-		H_[4] = 0xc3d2e1f0;
-		done_ = false;
-	}
 	/**
 		@param buf [in] buffer(64byte)
 	*/
@@ -135,6 +240,25 @@ public:
 	{
 		clear();
 	}
+	void clear()
+	{
+		static const uint32_t tbl[] = {
+			0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6
+		};
+		for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(tbl); i++) {
+			for (int j = 0; j < 20; j++) {
+				K_[i * 20 + j] = tbl[i];
+			}
+		}
+		totalSize_ = 0;
+		roundBufSize_ = 0;
+		H_[0] = 0x67452301;
+		H_[1] = 0xefcdab89;
+		H_[2] = 0x98badcfe;
+		H_[3] = 0x10325476;
+		H_[4] = 0xc3d2e1f0;
+		done_ = false;
+	}
 	void update(const void *_buf, size_t bufSize)
 	{
 		const char *buf = (const char *)_buf;
@@ -177,48 +301,21 @@ public:
 		clear();
 		return outByteSize;
 	}
-#if 0
-	void update(const std::string& buf)
-	{
-		update(buf.c_str(), buf.size());
-	}
-	std::string digest(const char *buf, size_t bufSize)
-	{
-		assert(!done_);
-		update(buf, bufSize);
-		term(roundBuf_, roundBufSize_);
-		std::string ret =  get();
-		clear();
-		return ret;
-	}
-	std::string digest(const std::string& str = "")
-	{
-		return digest(str.c_str(), str.size());
-	}
-	/**
-		convert to printable string
-	*/
-	std::string toString() const
-	{
-		std::string str;
-		char buf[32];
-		for (size_t i = 0; i < CYBOZU_NUM_OF_ARRAY(digest_); i++) {
-			uint32_t v = cybozu::Get32bitAsBE(&digest_[i]);
-			CYBOZU_SNPRINTF(buf, sizeof(buf), "%08x", v);
-			str += buf;
-		}
-		return str;
-	}
-	void get(char out[20]) const
-	{
-		memcpy(out, digest_, sizeof(digest_));
-	}
-	std::string get() const
-	{
-		return std::string(cybozu::cast<const char*>(&digest_[0]), sizeof(digest_));
-	}
-#endif
 };
 
 } // cybozu
 
+#endif
+
+namespace cybozu {
+
+/*
+	HMAC-SHA-1 (deprecated)
+	hmac must have 20 bytes buffer
+*/
+inline void hmac1(void *hmac, const void *key, size_t keySize, const void *msg, size_t msgSize)
+{
+	sha2_local::hmac<Sha1, 20, 64>(hmac, key, keySize, msg, msgSize);
+}
+
+} // cybozu
